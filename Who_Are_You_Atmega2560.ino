ce9e1845e43055b79383712f51b0959449184266
joebrooks <lcw3176@naver.com>
2021-05-27 00:38:06 +0900

    코드 분할

--- Who_Are_You_Atmega2560.ino
+++ Who_Are_You_Atmega2560.ino
@@ -1,10 +1,11 @@
 #include <SoftwareSerial.h>
 
-#define BLETXD 11
 #define BLERXD 10
-#define WIFITXD 13
+#define BLETXD 11
+
 #define WIFIRXD 12
-#define startFlag "<start>"
+#define WIFITXD 13
+
 #define endFlag "<end>"
 #define SENSOR 18
 // HIGH -> 분리, LOW -> 접촉
@@ -13,27 +14,33 @@ SoftwareSerial bluetoothSerial(BLERXD, BLETXD);
 SoftwareSerial wifiSerial(WIFIRXD, WIFITXD);
 String request;
 
+enum WiFiResult{
+  ConnectionWiFiError,
+  ConnectionServerError,
+};
+
+
 void setup() {
   bluetoothSerial.begin(9600);
   wifiSerial.begin(9600);
   Serial.begin(9600);
-  
-  attachInterrupt(SENSOR, DetachSensor, RISING);
-  attachInterrupt(SENSOR, AttachSensor, FALLING);
+//  attachInterrupt(SENSOR, DetachSensor, RISING);
+//  attachInterrupt(SENSOR, AttachSensor, FALLING);
 }
 
 void loop() {
+  bluetoothSerial.listen();
   
   if(bluetoothSerial.available())
   {
-    char recvData = bluetoothSerial.read();
-    request += recvData;
-    
+    request += (char)bluetoothSerial.read();
+
     if(request.endsWith(endFlag))
     {
-      String data = request.substring(request.indexOf(startFlag), request.length());
-      data.remove(0, 8); // <start> 제거
-      wifiSerial.write(data.c_str());
+      Serial.println(request);
+
+      wifiSerial.write(request.c_str());
+      request = "";
     }
   }
 
