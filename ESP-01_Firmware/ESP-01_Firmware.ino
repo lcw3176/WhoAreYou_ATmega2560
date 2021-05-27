15ad9a954ef7c29ba95489a0d53627d9dacc0e52
joebrooks <lcw3176@naver.com>
2021-05-28 02:34:27 +0900

    loop 중심 수정

--- ESP-01_Firmware/ESP-01_Firmware.ino
+++ ESP-01_Firmware/ESP-01_Firmware.ino
@@ -6,17 +6,19 @@
 #define RXD 3
 #define startFlag "<start>"
 #define endFlag "<end>"
-#define HOST "172.30.1.12"
+#define HOST "172.30.1.42"
 #define PATH "/auth/socket"
 
-// <start> / requestCommand / data / <end>
-enum BluetoothRequest{
+// 데이터 전달 형식: <start> / requestCommand / data / <end>
+// Ex: <start>/0/KT_2G_GIGA_WIFI/<end>
+enum RequestCommand{
   SetWiFiName,
   SetWiFiPassword,
   DisconnectWiFi,
   SetDeviceName,
   SetEmail,
   SetToken,
+  SensorChanged,
 };
 
 
@@ -24,16 +26,18 @@ WiFiClient wifiClient;
 WebSocketClient webSocketClient;
 SoftwareSerial wifiSerial(RXD, TXD);
 
-String ssid = "";
-String password = "";
+String ssid;
+String password;
 
-String token = "";
-String deviceName = "";
-String email = "";
-String request = "";
-bool isReadyToConnect = false;
+String token;
+String deviceName;
+String email;
+String request;
+bool isReadyToConnect;
 
+// Reset, boot x
 void setup() {
+  Init();
   wifiSerial.begin(9600);
   pinMode(0, OUTPUT); // 디버깅 포트
   digitalWrite(0, LOW);
@@ -45,16 +49,9 @@ void loop() {
   {    
     request += (char)wifiSerial.read();
 
-    if(request.length() > 7 && !request.startsWith(startFlag))
-    {
-      request = "";
-    }
-
     if(request.endsWith(endFlag))
     {
       digitalWrite(0, HIGH);
-      request = request.substring(request.indexOf(startFlag), request.length());
-      request.remove(0, 8); // <start> 제거
       AllocData(request);
       request = "";
     }
@@ -62,31 +59,30 @@ void loop() {
 
   if(isReadyToConnect)
   {
-        digitalWrite(0, LOW);
-        
+    digitalWrite(0, LOW);       
     if(ConnectWiFi())
     {
-        digitalWrite(0, LOW);
-        delay(2000);
-        digitalWrite(0, HIGH);
-        delay(2000);
-        
+      digitalWrite(0, HIGH);
       if(ConnectToServer())
       {
         digitalWrite(0, LOW);
-        delay(1000);
-        digitalWrite(0, HIGH);
-        delay(1000);
-        digitalWrite(0, LOW);
-        delay(1000);
-        digitalWrite(0, HIGH);
-        delay(1000);
-        SendToServer(email + "," + deviceName + ",true");
+        SendToServer("true");
       }
     }
 
-    isReadyToConnect = false;
+    Init();
   }
+}
+
+void Init()
+{
+  ssid = "";
+  password = "";
+  token = "";
+  deviceName = "";
+  email = "";
+  request = "";
+  isReadyToConnect = false;
 }
 
 void AllocData(String data)
@@ -116,6 +112,9 @@ void AllocData(String data)
       token = parameter;
       isReadyToConnect = true;
       break;
+    case SensorChanged:
+      SendToServer(parameter);
+      break;
     default:
       break;
   }
@@ -127,6 +126,7 @@ boolean ConnectToServer()
 {
   if(wifiClient.connect(HOST, 8080))
   {
+//    char* charToken = (char*)malloc(sizeof(char) * token.length());
     char charToken[150];
     strcpy(charToken, token.c_str());
     webSocketClient.path = PATH;
@@ -135,47 +135,57 @@ boolean ConnectToServer()
     
     if(webSocketClient.handshake(wifiClient))
     {
+//      free(charToken);
       return true;
     }
-   
-  }
 
+//   free(charToken);
+  }
+ 
   return false;
 }
 
 
 boolean ConnectWiFi()
 {
-  char id[64]; 
-  char pw[64]; 
-  strcpy(id, ssid.c_str());
-  strcpy(pw, password.c_str());
-
-  WiFi.begin(id, pw);
+//  char* charId = (char*)malloc(sizeof(char) * ssid.length()); 
+//  char* charPw = (char*)malloc(sizeof(char) * password.length()); 
+  if(WiFi.status() == WL_CONNECTED)
+  {
+    WiFi.disconnect(true);  
+  }
+  char charId[30];
+  char charPw[30];
+  strcpy(charId, ssid.c_str());
+  strcpy(charPw, password.c_str());
+  
+  WiFi.begin(charId, charPw);
   bool st = true;
   
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    if(st){
-      digitalWrite(0, LOW);
-    }
-
-    else{
-       digitalWrite(0, HIGH);
-    }
-
+    digitalWrite(0, st);
     st = !st;
+
   }
 
+//  free(charId);
+//  free(charPw);
+  
   return true;
 }
 
 
-boolean SendToServer(String data)
+boolean SendToServer(String state)
 {
+  // state
+  // true -> 분리됨
+  // false -> 접촉됨
+
   if(wifiClient.connected())
   {
-    webSocketClient.sendData(data);
+    webSocketClient.sendData(email + "," + deviceName + "," + state);
+    
     return true;  
   }
   
